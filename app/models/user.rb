e77618ee23d3db4d9a5eaed2d8cb0520ff294313
slawekkaw <slawek.kawalek@gmail.com>
2013-06-05 20:36:19 +0200

    make a basic User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,14 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
   
   validates :name, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6 }
+  
+  validates_confirmation_of :password
+  validates :password_confirmation, presence: true
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
